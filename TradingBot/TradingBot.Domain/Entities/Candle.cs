9343dbe309183ec6330950c702318c855b8c589a
Farzad Sepehr <farzad.sep@hotmail.com>
2026-04-10 13:58:55 +0330

    feat: create new solution and projects relations, create first entity

---
+++ TradingBot/TradingBot.Domain/Entities/Candle.cs
@@ -0,0 +1,34 @@
+﻿namespace TradingBot.Domain.Entities
+{
+    public class Candle
+    {
+        public long Id { get; private set; }
+
+        public string Symbol { get; private set; } // BTCUSDT
+
+        public DateTime OpenTime { get; private set; }
+
+        public decimal Open { get; private set; }
+        public decimal High { get; private set; }
+        public decimal Low { get; private set; }
+        public decimal Close { get; private set; }
+
+        public decimal Volume { get; private set; }
+        private Candle() { } // EF
+
+        public Candle(string symbol, DateTime openTime,
+            decimal open, decimal high, decimal low, decimal close, decimal volume)
+        {
+            Symbol = symbol;
+            OpenTime = openTime;
+            Open = open;
+            High = high;
+            Low = low;
+            Close = close;
+            Volume = volume;
+        }
+    }
+}
+
+
+
